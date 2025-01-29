fefaacf6928f19598e7d5afda4dbc7fbb78592f1
Abdurahman Hussien <80628811+AbdurahmanHussien@users.noreply.github.com>
2025-01-29 15:17:33 +0200

    Update task1 sol.sql

--- task1 sol.sql
+++ task1 sol.sql
@@ -1,14 +1,14 @@
 CREATE TABLE manager_test1 (id NUMBER(3),
-							name VarChar2(50), 
-							age NUMBER(2), 
-							birth_date date ,
-							address varChar2(50));
+			    name VarChar2(50), 
+			     age NUMBER(2), 
+			    birth_date date,
+			   address varChar2(50));
 
 
 ALTER TABLE MANAGER_TEST1 DROP COLUMN address;
 
 ALTER TABLE MANAGER_TEST1 ADD  (city_address varchar2(50),
-                street varchar2(50));
+                                street varchar2(50));
 
 ALTER TABLE MANAGER_TEST1 RENAME COLUMN  name to full_name;
 
